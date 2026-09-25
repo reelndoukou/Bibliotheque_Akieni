2923c9e299dab6bd35b228de6bd53b6a4366adcf
reelndoukou <reelndoukou@gmail.com>
2026-09-25 18:16:49 +0200

    ajout de la variable render pour le deploiement du backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,6 +12,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 // Petit test au démarrage pour vérifier que la connexion fonctionne
